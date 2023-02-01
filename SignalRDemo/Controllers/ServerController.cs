bb0392d45ff2393c7b8d0b8b3c87492c499b96d4
Haseeb Ahmed <66886913+HaseebAhmed49@users.noreply.github.com>
2023-02-01 18:13:16 +0500

    Simple Demo Application using SignalR using ASP.net MVC

---
+++ SignalRDemo/Controllers/ServerController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using SignalRDemo.Hubs;
+using SignalRDemo.Models;
+
+namespace SignalRDemo.Controllers
+{
+    public class ServerController : Controller
+    {
+        private readonly IHubContext<NotificationHub> _notificationHub;
+
+        public ServerController(IHubContext<NotificationHub> notificationHub)
+        {
+            _notificationHub = notificationHub;
+        }
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Index(Notification model)
+        {
+            await _notificationHub.Clients.All.SendAsync("receiveMsg", model.Message);            
+            return View();
+        }
+    }
+}
---
+++ SignalRDemo/wwwroot/js/ClientNotification.js
@@ -0,0 +1,9 @@
+﻿"use strict"
+var connection = new signalR.HubConnectionBuilder().withUrl("/notificationHub").build();
+
+connection.start();
+connection.on("receiveMsg", function (msg) {
+    var li = document.createElement("li");
+    li.textContent = msg;
+    document.getElementById("msgList").appendChild(li);
+})
